7bb2cef2a312d817a984b86f022f8f47414b6d95
unknown <sealkevinboyabad@gmail.com>
2024-11-29 14:06:51 -0800

    Initial commit

---
+++ src/test/java/dev/sealkboy/kata_peajesyvehiculos/KataPeajesyvehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.kata_peajesyvehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataPeajesyvehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
