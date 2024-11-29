cb3d6e0aedb92e552bf193ff969283541dbed8aa
unknown <sealkevinboyabad@gmail.com>
2024-11-29 14:38:27 -0800

    Crea la clase EstacionPeaje con métodos para agregar vehículos y calcular total de peajes

--- src/main/java/dev/sealkboy/kata_peajesyvehiculos/EstacionPeaje.java
+++ src/main/java/dev/sealkboy/kata_peajesyvehiculos/EstacionPeaje.java
@@ -1,13 +1,24 @@
 package dev.sealkboy.kata_peajesyvehiculos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class EstacionPeaje {
+    private String nombre;
+    private String ciudad;
+    private double totalPeaje;
 
-@SpringBootApplication
-public class KataPeajesyvehiculosApplication {
+    public EstacionPeaje(String nombre, String ciudad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.totalPeaje = 0;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(KataPeajesyvehiculosApplication.class, args);
-	}
+    public void agregarVehiculo(Vehiculo vehiculo) {
+        double peaje = vehiculo.calcularPeaje();
+        totalPeaje += peaje;
+        System.out.println("Vehículo con placa " + vehiculo.placa + " ha pagado: $" + peaje);
+    }
 
+    public void mostrarReporte() {
+        System.out.println("Estación de Peaje: " + nombre + " - Ciudad: " + ciudad);
+        System.out.println("Total de peajes recolectados: $" + totalPeaje);
+    }
 }
