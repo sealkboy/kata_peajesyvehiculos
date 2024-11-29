d108ebd184de89fa9103306f66a5a082886847cd
unknown <sealkevinboyabad@gmail.com>
2024-11-29 14:41:58 -0800

    Añade pruebas unitarias para la clase Camion

--- src/test/java/dev/sealkboy/kata_peajesyvehiculos/CamionTest.java
+++ src/test/java/dev/sealkboy/kata_peajesyvehiculos/CamionTest.java
@@ -1,13 +1,14 @@
 package dev.sealkboy.kata_peajesyvehiculos;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class KataPeajesyvehiculosApplicationTests {
+class CamionTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void testCalcularPeaje() {
+        Vehiculo camion = new Camion("LMN-456", 4);
+        assertEquals(200, camion.calcularPeaje(), "Debe de pagar 200");
+    }
 }
