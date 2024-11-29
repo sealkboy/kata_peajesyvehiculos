4a0ee26b451c08a17c6a557da008831a5c234493
unknown <sealkevinboyabad@gmail.com>
2024-11-29 14:34:15 -0800

    Crea la clase Camion que extiende Vehiculo, incluye campo numEjes y define calcularPeaje

--- src/main/java/dev/sealkboy/kata_peajesyvehiculos/Camion.java
+++ src/main/java/dev/sealkboy/kata_peajesyvehiculos/Camion.java
@@ -1,5 +1,15 @@
 package dev.sealkboy.kata_peajesyvehiculos;
 
-public class Camion {
-    
+public class Camion extends Vehiculo {
+    private int numEjes;
+
+    public Camion(String placa, int numEjes) {
+        super(placa);
+        this.numEjes = numEjes;
+    }
+
+    @Override
+    public double calcularPeaje() {
+        return numEjes * 50; 
+    }
 }
